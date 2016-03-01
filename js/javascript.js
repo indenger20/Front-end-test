964fe9077bf35835daf43ff9a899cf1e1ac4d431
yaroslav <indenger20@gmail.com>
2016-03-01 18:16:41 +0200

    Change jquery code in native js

--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,39 @@
-$(function() {
+"use strict";
 
-	$('#sendForm').on('click', function go(e) {
+var link = document.getElementById('sendForm');
+var textArea = document.getElementById('textForm');
+var new_comment_block = document.getElementsByClassName('all-comment')[0];
 
-		var text = $('#textForm').val();
-		console.log(text);
-		$('.all-comment').append(function(indx, val) {
-			if($('#textForm').val() != '') {
-				return '<div class="b-comment-line"><div class="b-comment-line__note"><p class="b-comment-line__note-text">'+text+'<p></div></div>';
-			}else if($('#textForm').val('')){
-				return alert('Введите текст');
-			}
-		})
-		function foo() {
-			$('#textForm').val('');
-		}
-		foo();
-		e.preventDefault();
-	});
+link.addEventListener('click', function(e) {
+	
+	if(textArea.value !== '') {
+		console.log(new_comment_block);
+		var myText = textArea.value;
+		var elem =  document.createElement('div');
+		elem.className = 'b-comment-line';
+		elem.innerHTML='<div class="b-comment-line__note"><p class="b-comment-line__note-text">'+myText+'</p><div>';
+		new_comment_block.appendChild(elem);
+		textArea.value = '';
+		
+	}else {
+		alert('Введите текст');
+	}
 
-	var isCtrl = false;
-	$("#textForm").keyup(function (e) {
-		if(e.which == 17) isCtrl=false;
-	})
-	$("#textForm").keydown(function (e) {
-		if(e.which == 17) isCtrl=true;
-		if(e.which == 13 && isCtrl == true) {
-	 		 $('#sendForm').click();
-			
-		}
-	 
-	});
+	e.preventDefault();
+});
 
+var isCtrl = false;
+
+textArea.addEventListener('keyup', function(event) {
+	if(event.which == 17) {
+		isCtrl=false;
+	}
+})
+textArea.addEventListener('keydown', function(event) {
+	if(event.which == 17) {
+		isCtrl=true;
+	}
+	if(event.which == 13 && isCtrl == true) {
+		link.click();
+	}
 })
